ee7bfdbf50bf5bceba666f9b9a7a67b417ccd77e
feestjebouwen01 <vandoorenmathijs@gmail.com>
2024-06-10 12:32:57 +0200

    admin_view

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostListDetailfilter
+from .views import PostList, PostDetail, PostListDetailfilter, CreatePost, AdminPostDetail, EditPost, DeletePost
 from rest_framework.routers import DefaultRouter
 
 app_name = 'blog_api'
@@ -9,7 +9,12 @@ app_name = 'blog_api'
 # urlpatterns = router.urls
 
 urlpatterns = [
-   path('posts/', PostDetail.as_view(), name='detailcreate'),
-   path('search/', PostListDetailfilter.as_view(), name='postsearch'),
    path('', PostList.as_view(), name='listcreate'),
+   path('post/<str:pk>/', PostDetail.as_view(), name='detailcreate'),
+   path('search/', PostListDetailfilter.as_view(), name='postsearch'),
+   #Post Admin URLs
+   path('admin/create/', CreatePost.as_view(), name='createpost'),
+   path('admin/edit/postdetail/<int:pk>/', AdminPostDetail.as_view(), name='admindetailpost'),
+   path('admin/edit/<int:pk>/', EditPost.as_view(), name='editpost'),
+   path('admin/delete/<int:pk>/', DeletePost.as_view(), name='deletepost'),
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -47,13 +47,17 @@ class PostUserWritePermission(BasePermission):
 #         serializer_class = PostSerializer(post)
 #         return Response(serializer_class.data)
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = PostSerializer
+# class PostList(generics.ListCreateAPIView):
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = PostSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        return Post.objects.filter(author=user)
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Post.objects.filter(author=user)
+
+class PostList(generics.ListAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
 # class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
 #     permission_classes = [PostUserWritePermission]
@@ -64,16 +68,44 @@ class PostList(generics.ListCreateAPIView):
 #         item = self.kwargs.get('pk')
 #         return get_object_or_404(Post, slug=item)
 
-class PostDetail(generics.ListAPIView):
+class PostDetail(generics.RetrieveAPIView):
     serializer_class = PostSerializer
 
-    def get_queryset(self):
-        slug = self.request.query_params.get('slug', None)
-        print(slug)
-        return Post.objects.filter(slug=slug)
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get('pk')
+        return get_object_or_404(Post, slug=item)
+
+# class PostDetail(generics.ListAPIView):
+#     serializer_class = PostSerializer
+
+#     def get_queryset(self):
+#         slug = self.request.query_params.get('slug', None)
+#         print(slug)
+#         return Post.objects.filter(slug=slug)
 
 class PostListDetailfilter(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['^slug']
+
+
+class CreatePost(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class AdminPostDetail(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class EditPost(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+class DeletePost(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()

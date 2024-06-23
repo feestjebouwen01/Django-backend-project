68e08793b811e4614a852263531ec7a5eb96d18a
feestjebouwen01 <vandoorenmathijs@gmail.com>
2024-06-23 07:35:35 +0200

    customer file

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    customer_id = models.BigIntegerField(unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
